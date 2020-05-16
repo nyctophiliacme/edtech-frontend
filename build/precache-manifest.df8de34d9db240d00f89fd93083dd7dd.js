6af011ec00d8d044ada0049c9e5196c4279e05f4
devanshuSiemens <devanshu.avionics@gmail.com>
2020-05-16 16:58:21 +0530

    buid folder

---
+++ build/precache-manifest.df8de34d9db240d00f89fd93083dd7dd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e9fe6f76aea5fd69278accb1f6ffc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e6d4ded64e4691834ef",
+    "url": "/static/css/main.5936d10c.chunk.css"
+  },
+  {
+    "revision": "5a92a9530882ee0877b5",
+    "url": "/static/js/2.a34d41bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a34d41bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6d4ded64e4691834ef",
+    "url": "/static/js/main.0016d9a6.chunk.js"
+  },
+  {
+    "revision": "1ffadb0947cb7d6c4011",
+    "url": "/static/js/runtime-main.ee7c047f.js"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "dad65c74d61eada4566f121fd8ef3d34",
+    "url": "/static/media/illustration-01.dad65c74.png"
+  },
+  {
+    "revision": "f8ceb17b5d295d7a2a99dafec9348350",
+    "url": "/static/media/illustration-02.f8ceb17b.png"
+  },
+  {
+    "revision": "010d6f4a5a0707bfde25af91fd9f96c9",
+    "url": "/static/media/illustration-03.010d6f4a.png"
+  },
+  {
+    "revision": "234e189de1c4dfb71a142d271c2ee895",
+    "url": "/static/media/top-banner-img.234e189d.png"
+  }
+]);
