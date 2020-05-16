cc29f8162fbff166a5c86f2337debb54a78eaced
bpatro <bpatro@motorola.com>
2020-05-16 15:47:34 +0530

    Added home page link to logo.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
-import './header.css'
+import "./header.css";
 
 const header = () => {
-   return (
-      <header className="logo-header">
-      <img src={logo} alt="logo" />
+  return (
+    <header className="logo-header">
+      <Link to="/">
+        <img src={logo} alt="logo" />
+      </Link>
     </header>
-   
-   );
-}
+  );
+};
 export default header;
