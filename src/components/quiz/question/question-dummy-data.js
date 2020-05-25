b636ad67f384e9a6cda651723480546feec06d31
devanshuSiemens <devanshu.avionics@gmail.com>
2020-05-25 18:25:50 +0530

    question dummy data

---
+++ src/components/quiz/question/question-dummy-data.js
@@ -0,0 +1,49 @@
+export const quesionList=[
+    {
+      "id": "1",
+      "question_text": "what is the cap of india?",
+      "difficulty_level": "Easy",
+      "explanation": "ye bhi nahi pata.. ",
+      "question_choices":[{
+        "choice_text":"delhi",
+        "choice_img_url":"",
+        "is_right_option":true
+      },
+      {
+        "choice_text":"Bombay",
+        "choice_img_url":"",
+        "is_right_option":false
+      },
+      {
+        "choice_text":"Calcuta",
+        "choice_img_url":"",
+        "is_right_option":false
+      },
+      {
+        "choice_text":"bangalore",
+        "choice_img_url":"",
+        "is_right_option":false
+      }]
+    },
+    {
+      "id": "2",
+      "question_text": "what is english of 'kadak chai' ?",
+      "difficulty_level": "Easy",
+      "explanation": "take english coaching ....",
+      "question_choices":[{
+        "choice_text":"strong tea",
+        "choice_img_url":"",
+        "is_right_option":true
+      },
+      {
+        "choice_text":"healthy tea",
+        "choice_img_url":"",
+        "is_right_option":false
+      },
+      {
+        "choice_text":"whisky",
+        "choice_img_url":"",
+        "is_right_option":false
+      }]
+    }
+  ]
