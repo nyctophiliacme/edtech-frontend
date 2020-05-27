970f49a874e4da2a578a2da311d1aa3cb1624bfd
devanshuSiemens <devanshu.avionics@gmail.com>
2020-05-27 12:49:19 +0530

    comit for question screen

--- src/components/quiz/question/question.js
+++ src/components/quiz/question/question.js
@@ -2,26 +2,48 @@ import React, { Component } from "react";
 import "./question.css";
 import { check } from "../../../assets/images/check.png";
 class Question extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          questionDetails: null,
-          isSubmited: false,
-          slectedOption:null
-        };
-      }
-    
-      componentDidMount() {
-        this.setState({
-          questionList: this.props.question,
-        });
-      }
-      render(){
-          return(
-              <div className="question-container" >
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSubmited: false,
+      slectedOption: null,
+    };
+  }
 
-              </div>
-          )
-      }
+  render() {
+    return (
+      <div className="question-container">
+        <div className="question-header-container">
+    <div className="question-header-rightblock">{`Q${this.props.activeQuestion}`}</div>
+          <div className="question-header-leftblock">
+            {this.props.activeQuestion>1?<div onClick={this.props.prevClick}>PREV</div>:""}
+            {this.props.activeQuestion<this.props.totalQuestions?<div onClick={this.props.nextClick}>NEXT</div>:""} 
+          </div>
+        </div>
+        <div className="question-text">
+          {this.props.questionDetails?.question_text}
+        </div>
+        {this.props.questionDetails ? (
+          <div >
+            {this.props.questionDetails?.question_choice.map(
+              (option, index) => {
+                let charIndex = 97 + index;
+                return (
+                  <div key={index} className="question-option-container">
+                    <div className="option-index">
+                      {String.fromCharCode(charIndex)}
+                    </div>
+                    <div className="option-text">{option.choice_text}</div>
+                  </div>
+                );
+              }
+            )}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    );
+  }
 }
-export default Question
+export default Question;
--- src/components/quiz/quiz.js
+++ src/components/quiz/quiz.js
@@ -1,34 +1,65 @@
 import React, { Component } from "react";
 import QuestionList from "./question-list/question-list";
 import { getQuestions } from "../../services/questionService";
-
+import Question from "./question/question";
+import "./quiz.css";
 class Quiz extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      questionList:[]
-    }
+      questionList: [],
+      activeQuestion: 1,
+    };
   }
-  componentDidMount(){
+  componentDidMount() {
     this.getQuestions();
   }
-  getQuestions(){
-    getQuestions(48).then((response)=>{
+  getQuestions() {
+    getQuestions(48)
+      .then((response) => {
         this.setState({
-          questionList:response.data
-        })
-    }).catch((error)=>{
-      console.log(error)
-    })
+          questionList: response.data,
+          activeQuestion: 1,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
   render() {
-    console.log(this.props.location.pathname);
-    return(
+    return (
       <>
-      <div className="quiz-header"></div>
-      <QuestionList questionList={this.state.questionList}/>
+        <div className="quiz-header"></div>
+        <div className="quiz-container">
+          <QuestionList
+            questionList={this.state.questionList}
+            activeQuestion={this.state.activeQuestion}
+            click={(questionNumber) => {
+              this.setState({
+                activeQuestion: questionNumber,
+              });
+            }}
+          />
+          <Question
+            activeQuestion={this.state.activeQuestion}
+            totalQuestions={this.state.questionList.length}
+            questionDetails={
+              this.state.questionList[this.state.activeQuestion - 1]
+            }
+            prevClick={() =>
+              this.setState({
+                activeQuestion: this.state.activeQuestion - 1,
+              })
+            }
+            nextClick={() =>
+              this.setState({
+                activeQuestion: this.state.activeQuestion + 1,
+              })
+            }
+          />
+        </div>
       </>
-    )
+    );
   }
 }
-export default Quiz; 
+export default Quiz;
