3a5f2ea9c13ad22ea093e773d4e1f332bfa0ae26
bpatro <bpatro@motorola.com>
2020-05-30 21:28:47 +0530

    Removed unwanted package.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route, Redirect } from "react-router-dom";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import PrivacyPolicy from "./components/privacy_policy/privacy_policy";
@@ -8,37 +8,31 @@ import TermAndCondition from "./components/term_condition/term_condition";
 import MarketingV2 from "./components/marketing_v2/marketing_v2";
 import ExamHome from "./components/exam/examHome/exam_home";
 import PracticeHome from "./components/practice/practice-home/practice-home";
-import Notifications from 'react-notify-toast';
-import { createStore } from '@spyna/react-store';
+import Notifications from "react-notify-toast";
 import Pricing from "./components/pricing/pricing";
 import Quiz from "./components/quiz/quiz";
 
 import "./App.css";
 
-
 function App() {
   return (
     <>
-    <Notifications />
-    <Header></Header>
-    <Switch>
-      <Route exact path='/' component={MarketingV2}/>
-      <Route path='/privacy_policy' component={PrivacyPolicy}/>
-      <Route path='/term_condition' component={TermAndCondition}/>
-      <Route path='/about_us' component={AboutUs}/>
-      <Route path='/exam/:examName/:defaultSection' component={ExamHome} />
-      <Route path='/practice/:examName' component={PracticeHome} />
-      <Route path='/pricing' component={Pricing}/>
-      <Route path='/quiz' component={Quiz}/>
-      <Redirect to="/" />
-    </Switch>
-    <Footer></Footer>
+      <Notifications />
+      <Header></Header>
+      <Switch>
+        <Route exact path="/" component={MarketingV2} />
+        <Route path="/privacy_policy" component={PrivacyPolicy} />
+        <Route path="/term_condition" component={TermAndCondition} />
+        <Route path="/about_us" component={AboutUs} />
+        <Route path="/exam/:examName/:defaultSection" component={ExamHome} />
+        <Route path="/practice/:examName" component={PracticeHome} />
+        <Route path="/pricing" component={Pricing} />
+        <Route path="/quiz" component={Quiz} />
+        <Redirect to="/" />
+      </Switch>
+      <Footer></Footer>
     </>
   );
 }
 
-const initialValue = {
-  isLoggedIn:false
-}
-
-export default createStore(App, initialValue);
+export default App;
