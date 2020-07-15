e298e99ba98bbf1c053e07fc988b76fc2ff5b2b7
devanshuSiemens <devanshu.avionics@gmail.com>
2020-07-15 20:32:19 +0530

    filter for subject tiles

--- src/components/course_library/subject_list/subject_list.js
+++ src/components/course_library/subject_list/subject_list.js
@@ -63,6 +63,46 @@ class SubjectList extends Component {
           exam_id: 1,
           exam_fullname: "Engineering common admission test",
         },
+        {
+          subject_id: 5,
+          subject_name: "Chemestry",
+          icon_url: "asa",
+          background_start_color: "#DD7B34",
+          background_end_color: "#E58A48",
+          exam_code: "net",
+          exam_id: 2,
+          exam_fullname: "National entrance exam",
+        },
+        {
+          subject_id: 6,
+          subject_name: "English",
+          icon_url: "asa",
+          background_start_color: "#A320B9",
+          background_end_color: "#AA2D7F",
+          exam_code: "ecat",
+          exam_id: 2,
+          exam_fullname: "National entrance exam",
+        },
+        {
+          subject_id: 7,
+          subject_name: "Maths",
+          icon_url: "asa",
+          background_start_color: "#1AAE8A",
+          background_end_color: "#00C067",
+          exam_code: "itpu",
+          exam_id: 3,
+          exam_fullname: "Engineering common admission test",
+        },
+        {
+          subject_id: 8,
+          subject_name: "Physics",
+          icon_url: "asa",
+          background_start_color: "#DB2D67",
+          background_end_color: "#E55748",
+          exam_code: "itpu",
+          exam_id: 3,
+          exam_fullname: "Engineering common admission test",
+        },
       ],
       sectionName: "Test Prep",
       subSectionName: "Engineering",
@@ -117,21 +157,29 @@ class SubjectList extends Component {
         </div>
         <div className="subject-tiles-container">
           {this.state.subjectList.map((subject) => {
-            return (
-              <div className="subject-detail-container">
-                <div
-                  className="subject-icon-container"
-                  style={ { background: `linear-gradient(to right,${ subject.background_start_color },${subject.background_end_color})` } }
-                >
-                  ss
+            if (
+              this.state.selectedExamId === 0 ||
+              this.state.selectedExamId === subject.exam_id
+            ) {
+              return (
+                <div className="subject-detail-container">
+                  <div
+                    className="subject-icon-container"
+                    style={{
+                      background: `linear-gradient(to right,${subject.background_start_color},${subject.background_end_color})`,
+                    }}
+                  >
+                    ss
+                  </div>
+                  <div className="subject-text-container">
+                    <span className="subject-name-highlight">
+                      {subject.subject_name}
+                    </span>
+                    {` for ${subject.exam_fullname}(${subject.exam_code.toUpperCase()})`}
+                  </div>
                 </div>
-                <div className="subject-text-container">
-                  <span className="subject-name-highlight">
-                    {subject.subject_name}
-                  </span>{`for ${subject.exam_fullname}(${subject.exam_code.toUpperCase()})`}
-                </div>
-              </div>
-            );
+              );
+            }
           })}
         </div>
       </div>
