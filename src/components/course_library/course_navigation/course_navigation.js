872da38582374384c34e6d567da722ff74a93537
bpatro <bpatro@motorola.com>
2020-07-15 11:10:46 +0530

    created the files.

---
+++ src/components/course_library/course_navigation/course_navigation.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+
+class CourseNavigation extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>
+      course Navigaton
+    </div>;
+  }
+}
+
+export default CourseNavigation;
