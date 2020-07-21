86a838c69ac9fa4864da729bea7eeaf9bc10099e
devanshuSiemens <devanshu.avionics@gmail.com>
2020-07-21 23:55:04 +0530

    changes for api

--- src/components/course_library/course_library.js
+++ src/components/course_library/course_library.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import SubjectList from "./subject_list/subject_list";
-import {
-  getAllCourses
-} from "../../services/courseService";
+import { getAllCourses } from "../../services/courseService";
 import "./course-library.css";
 import CourseNavigation from "./course_navigation/course_navigation";
 
@@ -11,12 +9,8 @@ class CourseLibrary extends Component {
     super(props);
     this.state = {
       courses: {},
-<<<<<<< HEAD
-      selectedCourseId: "",
-=======
       subjects: {},
       selectedCourseId: this.props.match.params.courseId,
->>>>>>> 8c9fc233d55849b70c841ec85ba56900ef5a9414
       selectedCourseName: "",
       selectedSectionName: "",
     };
@@ -31,32 +25,9 @@ class CourseLibrary extends Component {
   getCoursesList = () => {
     getAllCourses()
       .then((response) => {
-        this.setState(
-          {
-            courses: response.data,
-<<<<<<< HEAD
-            selectedCourseId: response.data[0].courses[0].id,
-            selectedCourseName: response.data[0].courses[0].course_title,
-            selectedSectionName: response.data[0].course_container_title,
-          }
-        );
-=======
-          },
-          this.getSubjectList()
-        );
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  getSubjectList = () => {
-    getAllSubjects()
-      .then((response) => {
         this.setState({
-          subjects: response.data,
+          courses: response.data,
         });
->>>>>>> 8c9fc233d55849b70c841ec85ba56900ef5a9414
       })
       .catch((error) => {
         console.log(error);
@@ -64,30 +35,6 @@ class CourseLibrary extends Component {
   };
 
   render() {
-<<<<<<< HEAD
-    console.log(
-      `Logged Output: CourseLibrary -> constructor -> courses`,
-      this.state.courses
-    );
-    console.log(
-      `Logged Output: SubjectLibrary -> render -> this.state.subjects`,
-      this.state.subjects
-    );
-    return (
-      <div>
-        <div className="course-library-header">Course Library</div>
-        <div className="course-library-container">
-          <CourseNavigation
-            courses={this.state.courses}
-            selectedCourseId={this.state.selectedCourseId}
-          />
-          <SubjectList
-            selectedCourseId={this.state.selectedCourseId}
-            selectedCourseName={this.state.selectedCourseName}
-            selectedSectionName={this.state.selectedSectionName}
-          />
-        </div>
-=======
     console.log(this.props.match.params.courseId);
     return (
       <div>
@@ -100,14 +47,13 @@ class CourseLibrary extends Component {
                 selectedCourseId={this.props.match.params.courseId}
               />
               <SubjectList
-                subjects={this.state.subjects}
+                selectedCourseId={this.state.selectedCourseId}
                 selectedCourseName={this.state.selectedCourseName}
                 selectedSectionName={this.state.selectedSectionName}
               />
             </div>
           </>
         ) : null}
->>>>>>> 8c9fc233d55849b70c841ec85ba56900ef5a9414
       </div>
     );
   }
