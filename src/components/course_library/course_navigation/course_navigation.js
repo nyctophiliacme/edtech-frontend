a7a333d52a1966893f03b1ab75a7c5a37f377cb7
bpatro <bpatro@motorola.com>
2020-07-22 18:11:10 +0530

    Updated mobile view.

--- src/components/course_library/course_navigation/course_navigation.js
+++ src/components/course_library/course_navigation/course_navigation.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { handleResize, debounce } from "../../../common/deviceDetection";
 import "./course-navigation.css";
 
 class CourseNavigation extends Component {
@@ -8,44 +9,106 @@ class CourseNavigation extends Component {
     this.state = {
       courseId: this.props.selectedCourseId,
       courses: this.props.courses,
+      bottomSheet: false,
+      checkDevice: handleResize(),
     };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    window.addEventListener(
+      "resize",
+      debounce(() => {
+        this.setState({ checkDevice: handleResize() });
+      }, 500)
+    );
+  }
+
+  toggleBottomSheet = () => {
+    this.setState({ bottomSheet: !this.state.bottomSheet });
+  };
 
   render() {
-    return (
-      <div className="course-navigation">
-        {this.state.courses.length !== 0
-          ? this.state.courses.map((category, index) => {
-              return (
-                <div key={index} className="section">
-                  <div className="section-title">
-                    {category.course_container_title}
+    const {
+      checkDevice: { isMobile },
+    } = this.state;
+    let overlay = (
+      <div>
+        <div className="course-navigation">
+          {this.state.courses.length !== 0
+            ? this.state.courses.map((category, index) => {
+                return (
+                  <div key={index} className="section">
+                    <div className="section-title">
+                      {category.course_container_title}
+                    </div>
+                    <div className="sub-section">
+                      {category.courses.map((subsection) => {
+                        return (
+                          <div
+                            className={`subsection-item ${
+                              parseInt(subsection.id) ===
+                              parseInt(this.props.selectedCourseId)
+                                ? "active"
+                                : ""
+                            }`}
+                            key={subsection.id}
+                          >
+                            <Link to={`/courses/${subsection.id}/`}>
+                              {subsection.course_title}
+                            </Link>
+                          </div>
+                        );
+                      })}
+                    </div>
                   </div>
-                  <div className="sub-section">
-                    {category.courses.map((subsection) => {
-                      return (
-                        <div
-                          className={`subsection-item ${
-                            parseInt(subsection.id) === parseInt(this.props.selectedCourseId)
-                              ? "active"
-                              : ""
-                          }`}
-                          key={subsection.id}
-                        >
-                          <Link to={`/courses/${subsection.id}/`}>
-                            {subsection.course_title}
-                          </Link>
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              );
-            })
-          : null}
+                );
+              })
+            : null}
+        </div>
+        <div
+          onClick={this.toggleBottomSheet}
+          className="btn-close"
+          aria-hidden="true"
+        >
+          {/* <span className="mdi mdi-close"></span>
+          <span className="sr-only">Close</span> */}
+        </div>
       </div>
+    );
+    // let navigationContent = (
+
+    // );
+    let obj = [];
+    obj.push({
+      title: "modify",
+      handler: () => {
+        window.alert("modify clicked!");
+      },
+    });
+    obj.push({
+      title: "delete",
+      handler: () => {
+        window.alert("delete clicked!");
+      },
+    });
+    console.log(isMobile);
+    return (
+      <>
+        {this.state.checkDevice.isMobile ? (
+          <div>
+            <button className="example" onClick={this.toggleBottomSheet}>
+              open bottom sheet!
+            </button>
+            {this.state.bottomSheet ? (
+              <div id="bottom-sheet" className="overlay" onClick={this.toggleBottomSheet}>
+                {overlay}{" "}
+              </div>
+            ) : null}
+          </div>
+        ) : (
+          overlay
+        )}
+      </>
     );
   }
 }
