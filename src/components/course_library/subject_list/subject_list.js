7c71cdc2814fc349161aff2c43213f5751967897
devanshuSiemens <devanshu.avionics@gmail.com>
2020-07-23 10:10:33 +0530

    not started and practice tab view fix

--- src/components/course_library/subject_list/subject_list.js
+++ src/components/course_library/subject_list/subject_list.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./subject-list.css";
 import { Link, withRouter } from "react-router-dom";
-import { messageService } from "../../../services/notifyComponentService";
 import {
   getAllSubjectForCourse,
   getAllExamForCourse,
