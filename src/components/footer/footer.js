4e892ba712df11296365175e89f6773e322913cb
bpatro <bpatro@motorola.com>
2020-07-23 12:26:57 +0530

    footer completed

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,47 +5,58 @@ import facebook from "../../assets/images/facebook.png";
 import youtube from "../../assets/images/youtube.png";
 
 const footer = () => {
-  const scrollToTop=() => {
+  const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth"
+      behavior: "smooth",
     });
-  }
-
+  };
 
   return (
     <div className="footer-container">
-      <div className="footer-container-left">
+      <div className="footer-column footer-first-column">
+        <div className="column-header">Company</div>
         <div className="footer-text" onClick={scrollToTop}>
-          <Link to="/about_us">ABOUT US</Link>
+          <Link to="/about_us">About Us</Link>
         </div>
         <div className="footer-text" onClick={scrollToTop}>
-          <Link to="/pricing">HOW TO PAY</Link>
+          <Link to="/pricing">Contact Us</Link>
         </div>
         <div className="footer-text" onClick={scrollToTop}>
-          <Link to="/privacy_policy">PRIVACY POLICY</Link>
+          <Link to="/privacy_policy">Privacy Policy</Link>
         </div>
         <div className="footer-text" onClick={scrollToTop}>
-          <Link to="/term_condition">TERMS &amp; CONDITIONS</Link>
+          <Link to="/term_condition">Terms &amp; Conditions</Link>
         </div>
       </div>
-      <div className="footer-container-right">
-        <div className="footer-contact" >
-        <a
-            href="mailto:info.superteacher@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            CONTACT US
-          </a>
-          <br/>
-          info.superteacher@gmail.com
-          </div>
-        <div className="footer-address">
-          131 B, DHA Phase 1,
-          <br />
-          Lahore Cantt, Lahore
+      <div className="footer-column footer-second-column">
+        <div className="column-header">Account</div>
+        <div className="footer-text" onClick={scrollToTop}>
+          <Link to="/about_us">Pricing and Plans</Link>
         </div>
+        <div className="footer-text" onClick={scrollToTop}>
+          <Link to="/about_us">Login</Link>
+        </div>
+        <div className="footer-text" onClick={scrollToTop}>
+          <Link to="/pricing">Forgot Password</Link>
+        </div>
+      </div>
+      <div className="footer-column footer-third-column">
+        <div className="column-header">Helpful Links</div>
+        <div className="footer-text" onClick={scrollToTop}>
+          <Link to="/about_us">How to pay</Link>
+        </div>
+        <div className="footer-text" onClick={scrollToTop}>
+          <Link to="/pricing">FAQs</Link>
+        </div>
+        <div className="footer-text" onClick={scrollToTop}>
+          <Link to="/privacy_policy">Blog</Link>
+        </div>
+        <div className="footer-text" onClick={scrollToTop}>
+          <Link to="/term_condition">Help</Link>
+        </div>
+      </div>
+      <div className="footer-column footer-container-right">
         <div className="footer-icons">
           <a
             href="https://www.facebook.com/superteacher.pk/"
@@ -60,6 +71,15 @@ const footer = () => {
             rel="noopener noreferrer"
           >
             <img className="youtube" src={youtube} alt="youtube" />
+          </a>
+        </div>
+        <div className="footer-contact">
+          <a
+            href="mailto:info.superteacher@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            info.superteacher@gmail.com
           </a>
         </div>
       </div>
