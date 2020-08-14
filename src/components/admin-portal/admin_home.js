fad61a92054410f7999028b1b6f2e77d21aa6a3a
bpatro <bpatro@motorola.com>
2020-08-14 13:29:15 +0530

    routing for admin.

--- src/components/admin-portal/admin_home.js
+++ src/components/admin-portal/admin_home.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import AdminLogin from "./admin_login";
+import AddChapter from "./chapter_add";
+import AddExam from "./exam_add";
+import AddSubject from "./subject_add";
 class AdminHome extends Component {
-    constructor(props) {
-      super(props);
-      
-    }
-render(){
-return <div>admin login</div>;
-}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Switch>
+        <Route path={`${this.props.match.path}/l`} component={AdminLogin} />
+        <Route
+          path={`${this.props.match.path}/addChapter`}
+          component={AddChapter}
+        />
+        <Route path={`${this.props.match.path}/addExam`} component={AddExam} />
+        <Route
+          path={`${this.props.match.path}/addSubject`}
+          component={AddSubject}
+        />
+        <Redirect to={`${this.props.match.path}/l`} />
+      </Switch>
+    );
+  }
 }
 
 export default AdminHome;
